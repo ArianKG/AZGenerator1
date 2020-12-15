3379167c5fac2075821e2028f9f115c53fde68bf
ArianKG <72304422+ArianKG@users.noreply.github.com>
2020-12-15 11:25:33 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function random_az() {
     var randomUpperCase = Math.floor(Math.random() * 2);
     var randomAZ;
-    if (randomUpperCase == 1)
-    {
+    if (randomUpperCase == 1) {
         randomAZ = String.fromCharCode(0 | Math.random() * 26 + 97);
     } else {
         randomAZ = String.fromCharCode(0 | Math.random() * 26 + 97).toUpperCase();
